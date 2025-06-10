95b3a85e0bfaf444e733a572dfe5c6a62364b182
bixiphel <allen131@rangers.uwp.edu>
2025-06-10 18:24:45 -0500

    Finished base layout.

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
-const board = document.getElementById('sudoku-board');
+var numSelected = null;
+var tileSelected = null;
 
-function createBoard() {
-    for (let i = 0; i < 81; i++) {
-        const input = document.createElement('input');
-        input.setAttribute('type', 'text');
-        input.setAttribute('maxlength', '1');
+var errors = 0;
 
-        input.addEventListener('input', () => {
-            const val = input.value;
-            if (!/^[1-9]?$/.test(val)) {
-                input.value = '';
-            }
-        });
+var board = [
+    "--74916-5",
+    "2---6-3-9",
+    "-----7-1-",
+    "-586----4",
+    "--3----9-",
+    "--62--187",
+    "9-4-7---2",
+    "67-83----",
+    "81--45---"
+]
 
-        board.appendChild(input);
-    }
+var solution = [
+    "387491625",
+    "241568379",
+    "569327418",
+    "758619234",
+    "123784596",
+    "496253187",
+    "934176852",
+    "675832941",
+    "812945763"
+]
+
+window.onload = function() {
+    setGame();
 }
 
-createBoard();
+function setGame() {
+    // Digits 1-9
+    for(let i = 1; i <= 9; i++) {
+        let number = document.createElement("div");
+        number.id = i;
+        number.innerText = i;
+        number.classList.add("number");
+        document.getElementById("digits").appendChild(number);
+    }
+
+    // Board 9x9
+    for(let r = 0; r < 9; r++) {
+        for(let c = 0; c < 9; c++) {
+            let tile = document.createElement("div")
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            document.getElementById("board").append(tile);
+        }
+    }
+}
